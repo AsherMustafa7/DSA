1561d4a881e954f560ec034e57184df2ef02e408
Asher Mustafa <ashermustafa7@gmail.com>
2026-07-17 13:43:45 +0530

    Time: 0 ms (100%), Space: 11.2 MB (55.05%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,7 +4,7 @@ public:
     {
         int count=0;
         int i =0;
-        int k=0;
+    
         while(i<nums.size())
         {
             if((nums[i%nums.size()])>nums[(i+1)%nums.size()])
