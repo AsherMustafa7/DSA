f5c27791abb6944e38230565a79d0d3b6f43b671
Asher Mustafa <ashermustafa7@gmail.com>
2026-07-18 21:07:35 +0530

    Time: 4 ms (34.74%), Space: 10 MB (11.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) 
-    {
-        if(s.size()!=t.size())
-        {
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
             return false;
         }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-
-        return s == t;
+        unordered_map<char,int> s1;
+        for(char x : s){
+            s1[x]++;
+        }
+        for(char c : t){
+            --s1[c];
+            if(s1[c]<0){
+                return false;
+            }
+        }
+        return true;
     }
 };
