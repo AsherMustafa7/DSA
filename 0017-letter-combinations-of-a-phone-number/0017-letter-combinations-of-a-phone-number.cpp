74998f148b902a69cd4534fdb8ef9fe34d6dca8d
Asher Mustafa <ashermustafa7@gmail.com>
2026-07-19 15:11:59 +0530

    Time: 0 ms (100%), Space: 9.2 MB (78.3%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,38 +2,37 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) 
     {
-        unordered_map<char,string>v;
-        v['1']="";
-        v['2']="abc";
-        v['3']="def";
-        v['4']="ghi";
-        v['5']="jkl";
-        v['6']="mno";
-        v['7']="pqrs";
-        v['8']="tuv";
-        v['9']="wxyz";
+        unordered_map<char,string>mp;
+        mp['1']="";
+        mp['2']="abc";
+        mp['3']="def";
+        mp['4']="ghi";
+        mp['5']="jkl";
+        mp['6']="mno";
+        mp['7']="pqrs";
+        mp['8']="tuv";
+        mp['9']="wxyz";
         vector<string>vf;
-        finds(vf,v,"",v[digits[0]],0,digits);
+        finds(vf,mp,"",0,digits);
         
         return vf;
     }
-    void finds(vector<string>&vf,unordered_map<char,string>&v,string strm, string f,int index,string &digits)
+    void finds(vector<string>& vf,unordered_map<char,string> &mp,string str,int index,string &digits)
     {
-        if(strm.size()==digits.size())
+        if(str.size()==digits.size())
         {
-            vf.push_back(strm);
+            vf.push_back(str);
             return;
         }
         if(index==digits.size())
         {
             return;
         }
-        for(int i =0; i <f.size();i++)
+        for(int i=0; i <mp[digits[index]].size();i++)
         {
-            if(index!=digits.size())
-            finds(vf,v,strm+f[i],v[digits[index+1]],index+1,digits);
-            finds(vf,v,strm,v[digits[index]],index+1,digits);
+            str.push_back(mp[digits[index]][i]);
+            finds(vf,mp,str,index+1,digits);
+            str.pop_back();
         }
-        return;
     }
 };
