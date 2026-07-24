41da1b93cda3bfaa2859102a00d2858539820de0
Asher Mustafa <ashermustafa7@gmail.com>
2026-07-24 16:41:41 +0530

    Time: 23 ms (54.07%), Space: 17.4 MB (55.77%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,90 +1,54 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) 
+    vector<vector<int>> fourSum(vector<int>& nums, int target)
     {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> vf;
-        long long sum=0;
-        for(int i =0; i <nums.size()-1;i++)
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        for(int i = 0; i < n - 3; i++)
         {
             if(i > 0 && nums[i] == nums[i - 1])
-            continue;
-            for(int j=i+1; j<nums.size()-2;j++)
+                continue;
+
+            for(int j = i + 1; j < n - 2; j++)
             {
                 if(j > i + 1 && nums[j] == nums[j - 1])
-                continue;
-                int k=j+1;
-                int l=nums.size()-1;
-                while(k<l)
+                    continue;
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while(k < l)
                 {
-                    sum =(1LL*nums[i])+nums[j]+nums[k]+nums[l];
-                    if(1ll*(sum)==1ll*target)
+                    long long sum = 1LL * nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if(sum == target)
                     {
-                        vf.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+
                         k++;
                         l--;
-                        if(k<l)
-                        {
-                            while(nums[k]==nums[k-1])
-                            {
-                                k++;
-                                if(k>=l)
-                                break;
-                            }
-                            while(nums[l]==nums[l+1])
-                            {
-                                l--;
-                                if(k>=l)
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            break;
-                        }
-                        
+
+                        while(k < l && nums[k] == nums[k - 1])
+                            k++;
+
+                        while(k < l && nums[l] == nums[l + 1])
+                            l--;
                     }
-                    else if(1ll*(sum)<1ll*target)
+                    else if(sum < target)
                     {
                         k++;
-                        if(k<l)
-                        {
-                            while(nums[k]==nums[k-1])
-                            {
-                                    k++;
-                                    if(k>=l)
-                                    break;
-                            }
-                        }
-                        else
-                        {
-                            break;
-                        }
-                        
                     }
-                    else if(1ll*(sum)>1ll*target)
+                    else
                     {
                         l--;
-                        if(k<l)
-                        {
-                            while(nums[l]==nums[l+1])
-                            {
-                                l--;
-                                if(k>=l)
-                                break;
-                            }
-                        }
-                        
-                        else
-                        {
-                            break;
-                        }
-                        
                     }
                 }
             }
         }
-        
-        return vf;
+
+        return ans;
     }
 };
