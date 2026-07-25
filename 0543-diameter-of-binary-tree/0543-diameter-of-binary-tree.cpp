e9a1a822bd2d9f6886653304de4a1b4cefb6a612
Asher Mustafa <ashermustafa7@gmail.com>
2026-07-25 15:43:31 +0530

    Time: 0 ms (100%), Space: 23.6 MB (94.43%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,22 +11,21 @@
  */
 class Solution {
 public:
-
-    int diameterOfBinaryTree(TreeNode* root) {
-        int maxi= 0;
-        int height =cal(root, maxi);
-        return maxi;
+    int diameterOfBinaryTree(TreeNode* root) 
+    {
+        int maxi=INT_MIN;
+        cal(root,maxi);
+        return maxi-2;
     }
-    int cal(TreeNode* root,int &maxi)
+    int cal(TreeNode* root, int &maxi)
     {
         if(root==NULL)
         {
             return 0;
         }
-        int lh= cal(root->left,maxi);
-        int rh = cal(root->right,maxi);
-        maxi= max(maxi,lh+rh);// Remember we are giving not the height, we are giving the width , so look at the Node 1 , the left height is 2 the right is 1, so lh+rh =2+1.
-        return 1+max(lh,rh);
+        int l=1+cal(root->left,maxi);
+        int r=1+cal(root->right,maxi);
+        maxi=max(maxi,l+r);
+        return max(l,r);
     }
-    
 };
