e116216d9188fac4990cafbfad7eec5ddb43fca3
Asher Mustafa <ashermustafa7@gmail.com>
2026-07-27 16:26:27 +0530

    Time: 104 ms (5.83%), Space: 95.8 MB (25.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,59 +1,49 @@
-class Solution {
+class Solution 
+{
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
     {
-        // Always binary search the smaller array
-        if(nums1.size() > nums2.size())
-            return findMedianSortedArrays(nums2, nums1);
-
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-
-        int low = 0;
-        int high = n1;
-
-        while(low <= high)
+        vector<int>v;
+        int r= 0;
+        int l=0;
+        while(l<nums1.size() && r<nums2.size())
         {
-            // Partition in nums1
-            int cut1 = (low + high) / 2;
-
-            // Partition in nums2
-            int cut2 = (n1 + n2 + 1) / 2 - cut1;
-
-            // Elements just left and right of the partitions
-
-            int L1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int R1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
-
-            int L2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-            int R2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
-
-            // Correct partition found
-            if(L1 <= R2 && L2 <= R1)
+            if(nums1[l]<=nums2[r])
             {
-                // Even total length
-                if((n1 + n2) % 2 == 0)
-                {
-                    return (max(L1, L2) + min(R1, R2)) / 2.0;
-                }
-
-                // Odd total length
-                return max(L1, L2);
+                v.push_back(nums1[l]);
+                l++;
             }
-
-            // Too many elements taken from nums1
-            else if(L1 > R2)
-            {
-                high = cut1 - 1;
-            }
-
-            // Too few elements taken from nums1
             else
             {
-                low = cut1 + 1;
+                v.push_back(nums2[r]);
+                r++;
             }
         }
-
-        return 0;
+        if(l<nums1.size())
+        {
+            while(l<nums1.size())
+            {
+                v.push_back(nums1[l]);
+                l++;
+            }
+        }
+        if(r<nums2.size())
+        {
+            while(r<nums2.size())
+            {
+                v.push_back(nums2[r]);
+                r++;
+            }
+        }
+        for(auto i:v)
+        {
+            cout<<i << " ";
+        }
+        if(v.size()%2!=0)
+        {
+            return (1.0)*(v[v.size()/2]);
+        }
+        
+        return ((double)v[v.size()/2] + (double)v[v.size()/2 - 1]) / 2.0;;
     }
 };
