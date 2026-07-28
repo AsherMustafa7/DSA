585f56288cb8423ad0244cd9edfb2c6d058b4cf5
Asher Mustafa <ashermustafa7@gmail.com>
2026-07-28 19:27:28 +0530

    Time: 11 ms (26.36%), Space: 14.5 MB (6.42%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,30 +1,65 @@
 class Solution {
 public:
-    int romanToInt(string s) 
+    int romanToInt(string s)
     {
-        map<char, int>mp;
-        mp['I']=1;
-        mp['V']=5;
-        mp['X']=10;
-        mp['L']=50;
-        mp['C']=100;
-        mp['D']=500;
-        mp['M']=1000;
-        int sum=0;
-        for(int i=0; i <s.size()-1;i++)
+        map<char, int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+
+        vector<string> str;
+
+        int l = 0;
+        int r = 0;
+
+        while (r < s.size() - 1)
         {
-            if(mp[s[i]]>=mp[s[i+1]])
+            if (mp[s[r + 1]] >= mp[s[r]])
             {
-                cout<< sum << endl;
-                sum=sum+mp[s[i]];
-                
+                r++;
             }
             else
             {
-                sum=sum-mp[s[i]];
+                str.push_back(s.substr(l, r - l + 1));
+                l = r + 1;
+                r++;
             }
         }
-        sum=sum +mp[s[s.size()-1]];
+
+        // Handle the last remaining part
+        if (l <= r)
+        {
+            str.push_back(s.substr(l, r - l + 1));
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < str.size(); i++)
+        {
+            string strs = str[i];
+
+            if (strs.size() == 1)
+            {
+                sum += mp[strs[0]];
+            }
+            else if (strs.size() == 2)
+            {
+                if (mp[strs[0]] < mp[strs[1]])
+                    sum += mp[strs[1]] - mp[strs[0]];
+                else
+                    sum += mp[strs[0]] + mp[strs[1]];
+            }
+            else
+            {
+                for (char c : strs)
+                    sum += mp[c];
+            }
+        }
+
         return sum;
     }
 };
