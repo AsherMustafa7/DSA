5a2ab5755ad8c5293993f6575d78e92f7954ab0b
Asher Mustafa <ashermustafa7@gmail.com>
2026-07-29 15:22:49 +0530

    Time: 247 ms (5.12%), Space: 49.6 MB (8.55%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,43 +9,76 @@
  */
 class Solution {
 public:
-    void findpath(TreeNode* root, TreeNode* target,
-                  vector<TreeNode*>& arr, vector<TreeNode*>& temp) {
-
-        if (root == nullptr)
-            return;
-
-        temp.push_back(root);
-
-        if (root == target) {
-            arr = temp;
-            temp.pop_back();
-            return;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        vector<TreeNode*>v1;
+        vector<TreeNode*>v2;
+        int flag=0;
+        findlca(root,p,v1,flag);
+        flag=0;
+        findlca(root,q,v2,flag);
+        int r1=v1.size()-1;
+        int r2=v2.size()-1;
+        for(auto it:v1)
+        {
+            cout<< it->val<<" ";
         }
-
-        findpath(root->left, target, arr, temp);
-        findpath(root->right, target, arr, temp);
-
-        temp.pop_back();
-    }
-
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-
-        vector<TreeNode*> ppath, qpath;
-        vector<TreeNode*> ptemp, qtemp;
-
-        findpath(root, p, ppath, ptemp);
-        findpath(root, q, qpath, qtemp);
-
-        TreeNode* ans = nullptr;
-
-        for (int i = 0; i < min(ppath.size(), qpath.size()); i++) {
-            if (ppath[i] == qpath[i])
-                ans = ppath[i];
+        cout<< endl;
+        for(auto it:v2)
+        {
+            cout<< it->val <<" ";
+        }
+        cout<<endl;
+        while(r1>=0 && r2>=0)
+        {
+            if(v1[r1]==v2[r2])
+            {
+                cout<<"r1"<<r1<<endl;
+                cout<<"r2"<<r2<<endl;
+                r1--;
+                r2--;
+            }
             else
-                break;
+            {
+                if(r1>0)
+                return v1[r1+1];
+                else
+                return v2[r2+1];
+            }
         }
-
-        return ans;
+        
+        
+        if(r1>=0)
+        {
+            cout<<"r1 "<<r1<<endl;
+            return v1[r1+1];
+        }
+        
+        if(r2>=0)
+        {
+            cout<<"r2 "<<r2<<endl;
+            return v2[r2+1];
+        }
+        
+        return NULL;
+    }
+    void findlca(TreeNode* root, TreeNode* f,vector<TreeNode*>&v,int &flag)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        if(root==f)
+        {
+            v.push_back(root);
+            flag=1;
+            return;
+        }
+        if(flag==0)
+        findlca(root->left,f,v,flag);
+        if(flag==0)
+        findlca(root->right,f,v,flag);
+        if(flag==1)
+        v.push_back(root);
     }
 };
