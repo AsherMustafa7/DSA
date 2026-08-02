735fbf3ebcd8195f61eff082e66664f572baaad7
Asher Mustafa <ashermustafa7@gmail.com>
2026-08-02 23:05:55 +0530

    Time: 0 ms (100%), Space: 10.5 MB (46.05%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -2,37 +2,6 @@ class Solution {
 public:
     bool stoneGame(vector<int>& piles) 
     {
-        int flag=false;
-        check(piles,0,piles.size()-1,0,0,flag,1);
-        return flag;
-        
-    }
-    void check(vector<int>& piles,int l,int r, int alice,int bob,int &flag, int flip)
-    {
-        if(l>=r)
-        {
-            if(alice>bob)
-            {
-                flag=true;
-            }
-            return;
-        }
-        if(flag==false)
-        {
-            if(flip==1)
-            {
-                check(piles,l+1,r,alice+piles[l],bob,flag,0);
-                check(piles,l,r-1,alice+piles[r],bob,flag,0);
-            }
-            else
-            {
-                check(piles,l+1,r,bob+piles[l],bob,flag,1);
-                check(piles,l,r-1,bob+piles[r],bob,flag,1);
-            }
-        }
-        else
-        {
-            return;
-        }
+        return true;
     }
 };
