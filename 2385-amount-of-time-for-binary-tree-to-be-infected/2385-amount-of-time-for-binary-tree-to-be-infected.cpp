6bd53866092f95d56dc69fa02aca73bd264bf51d
Asher Mustafa <ashermustafa7@gmail.com>
2026-08-03 14:21:58 +0530

    Time: 169 ms (52.62%), Space: 191.5 MB (64.83%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,75 +11,82 @@
  */
 class Solution {
 public:
-    int findMaxDistance(map<TreeNode*,TreeNode*>&mpp,TreeNode* target)
+    void find(TreeNode* root,int start,TreeNode* &target)
     {
+        if(root==NULL)
+        {
+            return;
+        }
+        if(target==nullptr)
+        {
+            if(root->val==start)
+            {
+                target=root;
+            }
+            find(root->left,start,target);
+            find(root->right,start,target);
+        }
+        else
+        {
+            return;
+        }
+        
+    }
+    int amountOfTime(TreeNode* root, int start) 
+    {
+        TreeNode* target=nullptr;
+        find(root,start,target);
+        unordered_map<TreeNode*,TreeNode*>mp;
         queue<TreeNode*>q;
+        store(root,mp);
         q.push(target);
-        map<TreeNode*,int> vis;
+        
+        int diff=0;
+        unordered_map<TreeNode*,int>vis;
         vis[target]=1;
-        int maxi=0;
         while(!q.empty())
         {
-            int sz=q.size();
-            int fl=0; // this flag is used to see if we are actually buring something, cause there might be a time where we are on the leaf node and
-            // the leaf has nothing to burn, so we cant really pudae maxi
-            for(int i =0;i<sz;i++)
+            int n =q.size();
+            while(n--)
             {
-                auto node= q.front();
+                
+                TreeNode* node=q.front();
                 q.pop();
-                if(node->left && !vis[node->left])
+                
+                if(mp.find(node)!=mp.end() && vis.find(mp[node])==vis.end())
                 {
-                    fl=1;
-                    vis[node->left]=1;
+                    q.push(mp[node]);
+                    vis[node]=1;
+                }
+                if(node->left!=NULL && vis.find(node->left)==vis.end())
+                {
                     q.push(node->left);
+                    vis[node]=1;
                 }
-                if(node->right && !vis[node->right])
+                if(node->right!=NULL && vis.find(node->right)==vis.end())
                 {
-                    fl=1;
-                    vis[node->right]=1;
                     q.push(node->right);
-                }
-                if(mpp[node]&& !vis[mpp[node]])
-                {
-                    fl=1;
-                    vis[mpp[node]]=1;
-                    q.push(mpp[node]);
+                    vis[node]=1;
                 }
             }
-            if(fl)maxi++;
-
+            diff++;
+            
         }
-        return maxi;
+        
+        return diff-1;
     }
-    TreeNode* bfsToMapParent(TreeNode* root, map<TreeNode*,TreeNode*> &mpp , int start){
-        queue<TreeNode*> q;
-        q.push(root);
-        TreeNode* res=NULL;
-        while(!q.empty())
+    void store(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&mp)
+    {
+        if(root==NULL){return;}
+        if(root->left)
         {
-            TreeNode* node=q.front();
-            if(node->val==start) {res=node;}
-            q.pop();
-            if(node->left!=NULL)
-            {
-                mpp[node->left]=node;
-                q.push(node->left);
-            }
-            if(node->right!=NULL)
-            {
-                mpp[node->right]=node;
-                q.push(node->right);
-            }
+            mp[root->left]=root;
         }
-        return res;
-    }
-
-    int amountOfTime(TreeNode* root, int start) {
-        //  So the first thing we need to do that is figure out the node which is infected, meaning we are only given the value in the "start"
-        // Also we need to map the parents out
-        map<TreeNode*,TreeNode*> mpp; // through this we will know if a node has a parent or not.
-        TreeNode* target= bfsToMapParent(root,mpp,start);
-        int maxi= findMaxDistance(mpp,target);
-        return maxi;
+        if(root->right)
+        {
+            mp[root->right]=root;
+        }
+        store(root->left,mp);
+        store(root->right,mp);
     }
 };
