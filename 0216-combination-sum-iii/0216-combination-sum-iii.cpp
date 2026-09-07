986b9f31403e70454f3799a7391e5edea2aeabb5
Asher Mustafa <ashermustafa7@gmail.com>
2026-09-07 20:52:34 +0530

    Time: 5 ms (5.53%), Space: 14.6 MB (5.91%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-
-    void solve(int index, int k, int n, vector<vector<int>> & ans, vector<int>& ds){
-        if(ds.size() == k){
-            if(n == 0){
-                ans.push_back(ds);
-                return;
+    void arrays(vector<vector<int>>&vec, vector<int>v, int i, int sum,int &target, int &k,int count)
+    {
+        if(count==k)
+        {
+            if(sum==target)
+            {
+                vec.push_back(v);
             }
+            return;
         }
-        if(n < 0) return;
-        for(int i=index; i<=9; i++){
-            ds.push_back(i);
-            solve(i+1, k, n-i, ans, ds);
-            ds.pop_back();
-        }
+        if(i==10 || sum>target)
+        {
+            return;
+        } 
+        
+        v.push_back(i); // 1 2 3 
+        arrays(vec,v,i+1,sum+i,target,k,count+1);
+        v.pop_back();
+        arrays(vec,v,i+1,sum,target,k,count);
+        
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        solve(1, k, n, ans, ds);
-        return ans;
+        vector<vector<int>>vec;
+        arrays(vec,{},1,0,n,k,0);
+        return vec;
     }
 };
